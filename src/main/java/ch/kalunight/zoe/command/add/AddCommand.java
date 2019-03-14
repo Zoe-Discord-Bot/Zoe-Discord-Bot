cf82254557d465cd63d21550e90b781d693af372
KaluNight <lucadasilva003@gmail.com>
2019-03-14 01:16:24 +0100

    Start impl help command for each command

--- src/main/java/ch/kalunight/zoe/command/add/AddCommand.java
+++ src/main/java/ch/kalunight/zoe/command/add/AddCommand.java
@@ -1,23 +1,42 @@
 package ch.kalunight.zoe.command.add;
 
+import java.util.function.BiConsumer;
+
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+
 import net.dv8tion.jda.core.Permission;
 
 public class AddCommand extends Command {
 
   public AddCommand() {
     this.name = "add";
+    this.arguments = "";
     Permission[] permissionRequired = {Permission.MANAGE_CHANNEL};
     this.userPermissions = permissionRequired;
     this.help = "Send info about add commands";
     Command[] commandsChildren = {new AddPlayerToTeam()};
     this.children = commandsChildren;
+    this.helpBiConsumer = getHelpMethod();
   }
   
   @Override
   protected void execute(CommandEvent event) {
-    //TODO: Add info to add command
   }
-
+  
+  private BiConsumer<CommandEvent, Command> getHelpMethod() {
+    return new BiConsumer<CommandEvent, Command>() {
+      @Override
+      public void accept(CommandEvent event, Command command) {
+        event.getTextChannel().sendTyping().complete();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Add commands :\n");
+        for(Command commandChildren : children) {
+          stringBuilder.append("`>" + name + " " + commandChildren.getName() + " " + commandChildren.getArguments() + "` : " + commandChildren.getHelp());
+        }
+        
+        event.reply(stringBuilder.toString());
+      }
+    };
+  }
 }

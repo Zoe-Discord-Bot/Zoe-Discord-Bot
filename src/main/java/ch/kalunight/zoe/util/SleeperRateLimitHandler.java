e2c147edca6f3f166d0babbf7444a4c8f73b1379
KaluNight <lucadasilva003@gmail.com>
2019-03-29 15:43:46 +0100

    Improve the system of SleeperRateLimitHandler

--- src/main/java/ch/kalunight/zoe/util/SleeperRateLimitHandler.java
+++ src/main/java/ch/kalunight/zoe/util/SleeperRateLimitHandler.java
@@ -1,9 +1,9 @@
 package ch.kalunight.zoe.util;
 
-import java.util.ArrayList;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ch.kalunight.zoe.model.BooleanSynchronisable;
 import net.rithms.riot.api.request.Request;
 import net.rithms.riot.api.request.RequestResponse;
 import net.rithms.riot.api.request.ratelimit.DefaultRateLimitHandler;
@@ -11,93 +11,69 @@ import net.rithms.riot.api.request.ratelimit.RespectedRateLimitException;
 
 public class SleeperRateLimitHandler extends DefaultRateLimitHandler {
 
-  private static final int MAX_REQUEST_BY_SECONDS = 20;
+  private static BooleanSynchronisable isRateLimited = new BooleanSynchronisable(false);
 
-  private static final int MAX_REQUEST_BY_MINUTES = 50;
+  private static DateTime rateLimitStart = DateTime.now();
 
-  private static final ArrayList<DateTime> secondList = new ArrayList<>();
-
-  private static final ArrayList<DateTime> minuteList = new ArrayList<>();
+  private static int rateLimitTimeInSeconds = -1;
 
   private static final Logger logger = LoggerFactory.getLogger(SleeperRateLimitHandler.class);
 
   @Override
   public void onRequestAboutToFire(Request request) throws RespectedRateLimitException {
 
-    synchronized(this) {
-      DateTime actualTime = DateTime.now();
-
-      secondList.add(actualTime);
-      minuteList.add(actualTime);
-      
-      boolean fireableRequest = false;
-      while(!fireableRequest) {
-
-        fireableRequest = true;
-        deleteOutOfRateLimiteTime(actualTime);
-
-        if(minuteList.size() >= MAX_REQUEST_BY_MINUTES || secondList.size() >= MAX_REQUEST_BY_SECONDS) {
-          fireableRequest = false;
-          try {
-            Thread.sleep(1000); //NOSONAR
-          } catch(InterruptedException e) {
-            logger.error(e.getMessage());
-            Thread.currentThread().interrupt();
-          }
+    if(isRateLimited.isValue()) {
+      try {
+        long msToSleep = (rateLimitTimeInSeconds * 1000) - (DateTime.now().getMillis() - rateLimitStart.getMillis());
+        if(msToSleep > 0) {
+          logger.info("Thread {} get blocked for {} ms for respect Rate limit", Thread.currentThread().getName(), msToSleep);
+          Thread.sleep(msToSleep);
+        }else {
+          logger.info("All threads can now work normally");
+          isRateLimited.setValue(false);
         }
-        actualTime = DateTime.now();
+      } catch(InterruptedException e) {
+        logger.error("Thread got interupt in onRequestDone : {}", e.getMessage());
+        Thread.currentThread().interrupt();
       }
     }
 
-    logger.debug("Request Launch : {}", request.getObject().getUrl());
-
+    logger.debug("Request send : {}", request.getObject().getUrl());
     super.onRequestAboutToFire(request);
   }
 
-  private void deleteOutOfRateLimiteTime(DateTime actualTime) {
-    ArrayList<Integer> listToDelete = new ArrayList<>();
-
-    for(int i = 0; i < secondList.size(); i++) {
-      if(secondList.get(i).isBefore(actualTime.plusSeconds(-1))) {
-        listToDelete.add(i);
-      }
-    }
-
-    for(int i = listToDelete.size(); i > 0; i--) {
-      secondList.remove((int) listToDelete.get(i - 1));
-    }
-
-    listToDelete = new ArrayList<>();
-
-    for(int i = 0; i < minuteList.size(); i++) {
-      if(minuteList.get(i).isBefore(actualTime.plusMinutes(-1))) {
-        listToDelete.add(i);
-      }
-    }
-
-    for(int i = listToDelete.size(); i > 0; i--) {
-      minuteList.remove((int) listToDelete.get(i - 1));
-    }
-  }
-  
   @Override
   public void onRequestDone(Request request) {
     super.onRequestDone(request);
-    
-    if(request.getResponse().getCode() == Request.CODE_ERROR_RATE_LIMITED) {
-      RequestResponse response = request.getResponse();
-      String retryAfterString = response.getHeaderField("Retry-After");
 
-      if (retryAfterString != null) {
-        int retryAfter = Integer.parseInt(retryAfterString);
-        logger.warn("Got rate limit error ! Wait ... : {} s", retryAfter);
-        try {
-          Thread.sleep(retryAfter * 1000l);
-        } catch(InterruptedException e) {
-          logger.error("Thread got interupt in onRequestDone : {}", e.getMessage());
-          Thread.currentThread().interrupt();
+    synchronized(isRateLimited) {
+      if(request.getResponse().getCode() == Request.CODE_ERROR_RATE_LIMITED) {
+        RequestResponse response = request.getResponse();
+        String retryAfterString = response.getHeaderField("Retry-After");
+
+        if (retryAfterString != null) {
+          isRateLimited.setValue(true);
+          setRateLimitTimeInSeconds(Integer.parseInt(retryAfterString));
+          setRateLimitStart(DateTime.now());
+          logger.info("Got rate limit response ! Block all request for {} s ...", getRateLimitTimeInSeconds());
         }
       }
     }
+  }
+
+  public static int getRateLimitTimeInSeconds() {
+    return rateLimitTimeInSeconds;
+  }
+
+  public static void setRateLimitTimeInSeconds(int rateLimitTimeInSeconds) {
+    SleeperRateLimitHandler.rateLimitTimeInSeconds = rateLimitTimeInSeconds;
+  }
+
+  public static DateTime getRateLimitStart() {
+    return rateLimitStart;
+  }
+
+  public static void setRateLimitStart(DateTime rateLimitStart) {
+    SleeperRateLimitHandler.rateLimitStart = rateLimitStart;
   }
 }

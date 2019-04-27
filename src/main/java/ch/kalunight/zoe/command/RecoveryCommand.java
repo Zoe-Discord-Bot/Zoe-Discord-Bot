e6ee3e9e7a7a0bd60f8af2be68b8a9c230ff498f
KaluNight <lucadasilva003@gmail.com>
2019-04-27 12:10:21 +0200

    Implement in CreatePlayerCommand in RecoveryCommand

--- src/main/java/ch/kalunight/zoe/command/RecoveryCommand.java
+++ src/main/java/ch/kalunight/zoe/command/RecoveryCommand.java
@@ -3,20 +3,33 @@ package ch.kalunight.zoe.command;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
-import com.jagrosh.jdautilities.command.impl.CommandClientImpl;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+
+import ch.kalunight.zoe.ServerData;
 import ch.kalunight.zoe.Zoe;
 import ch.kalunight.zoe.command.create.CreateCommand;
 import ch.kalunight.zoe.command.create.CreatePlayerCommand;
+import ch.kalunight.zoe.model.Player;
+import ch.kalunight.zoe.model.Server;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.rithms.riot.api.RiotApiException;
+import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
+import net.rithms.riot.constant.CallPriority;
+import net.rithms.riot.constant.Platform;
 
 public class RecoveryCommand extends Command {
 
+  private static final Logger logger = LoggerFactory.getLogger(RecoveryCommand.class);
   private final EventWaiter waiter;
 
   public RecoveryCommand(EventWaiter waiter) {
@@ -57,29 +70,80 @@ public class RecoveryCommand extends Command {
     if(e.getMessage().getContentRaw().equals("YES")) {
 
       e.getTextChannel().sendMessage("Alright, i start now ...").queue();
-      e.getTextChannel().sendMessage("By the power of chocolate moon cake ! Restore all theses lost players !").queueAfter(1, TimeUnit.SECONDS);
+      e.getTextChannel().sendMessage("By the power of chocolate moon cake ! Restore all theses lost things !").queueAfter(1, TimeUnit.SECONDS);
 
       List<Message> messages = e.getTextChannel().getIterableHistory().stream()
           .filter(m-> m.getAuthor().equals(user))
           .limit(1000)
           .collect(Collectors.toList());
 
-
-      for(Message potentialCreatePlayer : messages) {
-        String message = potentialCreatePlayer.getContentRaw();
-        if(message.startsWith(Zoe.BOT_PREFIX) && isCreatePlayerCommand(message)) {
-
-          User playerUser = CreatePlayerCommand.getMentionedUser(potentialCreatePlayer.getMentionedMembers());
-          if(playerUser == null) {
-            
+      try {
+        for(Message potentialCreatePlayer : messages) {
+          String message = potentialCreatePlayer.getContentRaw();
+          
+          if(message.startsWith(Zoe.BOT_PREFIX) && isCreatePlayerCommand(message)) {
+            executeCreatePlayerCommand(e.getGuild(), user, potentialCreatePlayer, message);
           }
           
+          
+          
+          
+        }
+        
+        
+      }catch(RiotApiException e1) {
+        if(e1.getErrorCode() == RiotApiException.SERVER_ERROR) {
+          e.getTextChannel().sendMessage("Riot server occured a issue. Please retry after doing a `>reset`.").queue();
+          logger.info("Riot api got an error : {}", e1.getMessage(), e1);
+        }else if(e1.getErrorCode() == RiotApiException.UNAVAILABLE) {
+          e.getTextChannel().sendMessage("Riot server are actually unavailable. Please retry later. (And make a `>reset` before.)").queue();
+          logger.info("Riot api is actually unavailable : {}", e1.getMessage(), e1);
+        }else {
+          e.getTextChannel().sendMessage("I got an unexpected error, Please retry later after doing a `>reset`.").queue();
+          logger.warn("Got an unexpected error : {}", e1.getMessage(), e1);
         }
       }
-
     }else {
       e.getTextChannel().sendMessage("Right, so i do nothing.").queue();
     }
+  }
+
+  private void executeCreatePlayerCommand(Guild guild, User user, Message potentialCreatePlayer, String message) throws RiotApiException {
+    User playerUser = CreatePlayerCommand.getMentionedUser(potentialCreatePlayer.getMentionedMembers());
+    if(playerUser == null) {
+      return;
+    }
+
+    Server server = ServerData.getServers().get(guild.getId());
+    if(CreatePlayerCommand.isTheGivenUserAlreadyRegister(user, server)) {
+      return;
+    }
+
+    List<String> listArgs = CreatePlayerCommand.getParameterInParenteses(getCreatePlayerArgsCommand(message));
+    if(listArgs.size() != 2) {
+      return;
+    }
+
+    String regionName = listArgs.get(0);
+    String summonerName = listArgs.get(1);
+
+    Platform region = CreatePlayerCommand.getPlatform(regionName);
+    if(region == null) {
+      return;
+    }
+
+    Summoner summoner;
+    try {
+      summoner = Zoe.getRiotApi().getSummonerByName(region, summonerName, CallPriority.NORMAL);
+    }catch(RiotApiException e1) {
+      if(e1.getErrorCode() != RiotApiException.DATA_NOT_FOUND) {
+        throw e1;
+      }
+      return;
+    }
+    
+    Player player = new Player(user, summoner, region, false);
+    server.getPlayers().add(player);
   }
 
   private boolean isCreatePlayerCommand(String command) {
@@ -91,6 +155,13 @@ public class RecoveryCommand extends Command {
 
     return messageInTreatment.equalsIgnoreCase(CreateCommand.USAGE_NAME)
         && command.substring(1).split(" ")[1].equals(CreatePlayerCommand.USAGE_NAME);
+  }
+
+  private String getCreatePlayerArgsCommand(String command) {
+    return command.substring(1)
+        .substring(CreateCommand.USAGE_NAME.length() + 1)
+        .substring(CreatePlayerCommand.USAGE_NAME.length() + 1)
+        .substring(1);
   }
 
   private void cancelRecovery(MessageReceivedEvent event) {

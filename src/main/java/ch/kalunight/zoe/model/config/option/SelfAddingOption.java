5f4237e4f354b7efec84b22a4d93d841a1675ce3
KaluNight <lucadasilva003@gmail.com>
2019-08-25 19:50:33 +0200

    Implementing reaction sys for options

--- src/main/java/ch/kalunight/zoe/model/config/option/SelfAddingOption.java
+++ src/main/java/ch/kalunight/zoe/model/config/option/SelfAddingOption.java
@@ -1,9 +1,17 @@
 package ch.kalunight.zoe.model.config.option;
 
+
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageReaction;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 
 public class SelfAddingOption extends ConfigurationOption {
 
@@ -23,12 +31,18 @@ public class SelfAddingOption extends ConfigurationOption {
         
         if(optionActivated == false) {
           
-          event.reply("Option in activation : **" + description + "**\n\n"
+          Message message = event.getTextChannel().sendMessage("Option in activation : **" + description + "**\n\n"
               + "This option will let player add them self in the system with the command ``>create player``, ``>delete player`` "
-              + "and this will activate the command ``>register``.");
+              + "and this will activate the command ``>register``.\n\n"
+              + ""
+              + ":white_check_mark: : Activate this option.\n"
+              + ":negative_squared_cross_mark: : Cancel the activation.").complete();
           
+          message.addReaction("✅").queue();
+          message.addReaction("❌").queue();
           
-          
+          waiter.waitForEvent(MessageReactionAddEvent.class,
+              e -> true, e -> checkOption(message, event.getMember()), 1, TimeUnit.MINUTES, () -> endTime());
         }else {
           
           
@@ -38,6 +52,34 @@ public class SelfAddingOption extends ConfigurationOption {
         }
       }
     };
+  }
+  
+  private void checkOption(Message message, Member member) {
+    
+    for(MessageReaction reaction: message.getReactions()) {
+      if(reaction.getReactionEmote().getName().equals("✅")) {
+        
+        for(User userReaction : reaction.getUsers()) {
+          if(userReaction.equals(member.getUser())) {
+            
+            
+            
+          }
+        }
+      }
+    }
+    
+  }
+  
+  
+  private void activateOption(Message message) {
+    
+  }
+  
+  private void endTime() {
+    
+    
+    
   }
 
   @Override

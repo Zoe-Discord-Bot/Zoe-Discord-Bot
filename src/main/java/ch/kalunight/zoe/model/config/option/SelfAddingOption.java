af65d7ad83b4c74e3dca07071c04bf0687e8e764
KaluNight <lucadasilva003@gmail.com>
2019-08-26 15:57:13 +0200

    Better use ButtonMenu

--- src/main/java/ch/kalunight/zoe/model/config/option/SelfAddingOption.java
+++ src/main/java/ch/kalunight/zoe/model/config/option/SelfAddingOption.java
@@ -1,17 +1,12 @@
 package ch.kalunight.zoe.model.config.option;
 
-
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-
-import net.dv8tion.jda.core.entities.Member;
+import com.jagrosh.jdautilities.menu.ButtonMenu;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageReaction;
-import net.dv8tion.jda.core.entities.User;
-import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 
 public class SelfAddingOption extends ConfigurationOption {
 
@@ -29,20 +24,20 @@ public class SelfAddingOption extends ConfigurationOption {
       @Override
       public void accept(CommandEvent event) {
         
-        if(optionActivated == false) {
+        if(!optionActivated) {
+          ButtonMenu.Builder choiceBuilder = new ButtonMenu.Builder();
           
-          Message message = event.getTextChannel().sendMessage("Option in activation : **" + description + "**\n\n"
+          choiceBuilder.setEventWaiter(waiter);
+          choiceBuilder.addChoices("✅","❌");
+          choiceBuilder.addUsers(event.getAuthor());
+          choiceBuilder.setText("Option in activation : **" + description + "**\n\n"
               + "This option will let player add them self in the system with the command ``>create player``, ``>delete player`` "
               + "and this will activate the command ``>register``.\n\n"
-              + ""
               + ":white_check_mark: : Activate this option.\n"
-              + ":negative_squared_cross_mark: : Cancel the activation.").complete();
+              + ":negative_squared_cross_mark: : Cancel the activation.");
           
-          message.addReaction("✅").queue();
-          message.addReaction("❌").queue();
+          choiceBuilder.setTimeout(2, TimeUnit.MINUTES);
           
-          waiter.waitForEvent(MessageReactionAddEvent.class,
-              e -> true, e -> checkOption(message, event.getMember()), 1, TimeUnit.MINUTES, () -> endTime());
         }else {
           
           
@@ -54,33 +49,6 @@ public class SelfAddingOption extends ConfigurationOption {
     };
   }
   
-  private void checkOption(Message message, Member member) {
-    
-    for(MessageReaction reaction: message.getReactions()) {
-      if(reaction.getReactionEmote().getName().equals("✅")) {
-        
-        for(User userReaction : reaction.getUsers()) {
-          if(userReaction.equals(member.getUser())) {
-            
-            
-            
-          }
-        }
-      }
-    }
-    
-  }
-  
-  
-  private void activateOption(Message message) {
-    
-  }
-  
-  private void endTime() {
-    
-    
-    
-  }
 
   @Override
   public String getChoiceText() {

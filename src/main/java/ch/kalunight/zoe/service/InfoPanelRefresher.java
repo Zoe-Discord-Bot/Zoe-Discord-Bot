37bdc304cf38ce89fd6803a8ee07659dd8549d52
KaluNight <lucadasilva003@gmail.com>
2019-11-04 10:56:14 +0100

    Bug fix when a message of infochannel has been deleted

--- src/main/java/ch/kalunight/zoe/service/InfoPanelRefresher.java
+++ src/main/java/ch/kalunight/zoe/service/InfoPanelRefresher.java
@@ -20,6 +20,7 @@ import ch.kalunight.zoe.model.player_data.Player;
 import ch.kalunight.zoe.model.player_data.Team;
 import ch.kalunight.zoe.util.InfoPanelRefresherUtil;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.PermissionOverride;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.exceptions.ErrorResponseException;
@@ -35,29 +36,29 @@ public class InfoPanelRefresher implements Runnable {
   private static final Logger logger = LoggerFactory.getLogger(InfoPanelRefresher.class);
 
   private Server server;
-  
+
   private boolean needToWait = false;
 
   public InfoPanelRefresher(Server server) {
     this.server = server;
   }
-  
+
   public InfoPanelRefresher(Server server, boolean needToWait) {
     this.server = server;
     this.needToWait = needToWait;
   }
-  
+
 
   @Override
   public void run() {
     try {
 
       if(server.getInfoChannel() != null) {
-        
+
         if(needToWait) {
           TimeUnit.SECONDS.sleep(3);
         }
-        
+
         for(Player player : server.getPlayers()) {
           player.refreshAllLeagueAccounts(CallPriority.NORMAL);
         }
@@ -65,13 +66,13 @@ public class InfoPanelRefresher implements Runnable {
         cleanRegisteredPlayerNoLongerInGuild();
         server.clearOldMatchOfSendedGamesIdList();
         deleteOlderInfoCards();
-        
+
         ArrayList<String> infoPanels = CommandEvent.splitMessage(refreshPannel());
 
         if(server.getInfoChannel() != null && server.getGuild().getTextChannelById(server.getInfoChannel().getId()) != null) {
 
           cleanInfoChannelCache();
-          
+
           if(infoPanels.size() < server.getControlePannel().getInfoPanel().size()) {
             int nbrMessageToDelete = server.getControlePannel().getInfoPanel().size() - infoPanels.size();
             for(int i = 0; i < nbrMessageToDelete; i++) {
@@ -103,6 +104,8 @@ public class InfoPanelRefresher implements Runnable {
           }catch (Exception e) {
             logger.warn("An unexpected error when cleaning info channel has occure.", e);
           }
+
+          clearLoadingEmote();
         } else {
           server.setInfoChannel(null);
           server.setControlePannel(new ControlPannel());
@@ -134,13 +137,36 @@ public class InfoPanelRefresher implements Runnable {
     List<Message> messagesToRemove = new ArrayList<>();
     for(Message partInfoPannel : server.getControlePannel().getInfoPanel()) {
       try {
-        partInfoPannel.addReaction(":clock1:").complete(); //TODO: to delete
+        partInfoPannel.addReaction("U+23F3").complete();
       }catch(ErrorResponseException e) {
         messagesToRemove.add(partInfoPannel);
       }
     }
     for(Message messageToRemove : messagesToRemove) {
       server.getControlePannel().getInfoPanel().remove(messageToRemove);
+    }
+  }
+
+  private void clearLoadingEmote() {
+    for(Message messageToClearReaction : server.getControlePannel().getInfoPanel()) {
+
+      Message retrievedMessage;
+      try {
+        retrievedMessage = server.getInfoChannel().retrieveMessageById(messageToClearReaction.getId()).complete();
+      } catch (ErrorResponseException e) {
+        logger.warn("Error when deleting loading emote : {}", e.getMessage(), e);
+        continue;
+      }
+
+      if(retrievedMessage != null) {
+        for(MessageReaction messageReaction : retrievedMessage.getReactions()) {
+          try {
+            messageReaction.removeReaction(Zoe.getJda().getSelfUser()).queue();
+          } catch (ErrorResponseException e) {
+            logger.warn("Error when removing reaction : {}", e.getMessage(), e);
+          }
+        }
+      }
     }
   }
 
@@ -225,13 +251,13 @@ public class InfoPanelRefresher implements Runnable {
     if(!card.getPlayers().isEmpty()) {
       Player player = card.getPlayers().get(0);
       if(!player.getLeagueAccountsInTheGivenGame(card.getCurrentGameInfo()).isEmpty()) {
-        
+
         LeagueAccount account = player.getLeagueAccountsInTheGivenGame(card.getCurrentGameInfo()).get(0);
-        
+
         if(account.getCurrentGameInfo() == null || account.getCurrentGameInfo().getGameId() != card.getCurrentGameInfo().getGameId()) {
           cardsToRemove.add(card);
         }
-        
+
       }else {
         cardsToRemove.add(card);
       }

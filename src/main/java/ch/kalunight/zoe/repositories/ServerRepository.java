17503d85a1755321a76bcb0179ac68a04d948fe0
KaluNight <lucadasilva003@gmail.com>
2019-11-28 23:10:17 +0100

    Start impl database

---
+++ src/main/java/ch/kalunight/zoe/repositories/ServerRepository.java
@@ -0,0 +1,11 @@
+package ch.kalunight.zoe.repositories;
+
+import ch.kalunight.zoe.model.Server;
+
+public class ServerRepository {
+
+  private ServerRepository() {
+    //Hide default public constructor
+  }
+  
+}

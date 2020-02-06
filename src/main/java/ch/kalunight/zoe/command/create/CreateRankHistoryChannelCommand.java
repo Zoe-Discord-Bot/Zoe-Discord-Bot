838f13f6664b4ac680a47fa09c076c72c93deea4
KaluNight <lucadasilva003@gmail.com>
2020-02-07 00:01:45 +0100

    Implement create rank history channel

--- src/main/java/ch/kalunight/zoe/command/create/CreateRankHistoryChannelCommand.java
+++ src/main/java/ch/kalunight/zoe/command/create/CreateRankHistoryChannelCommand.java
@@ -3,19 +3,26 @@ package ch.kalunight.zoe.command.create;
 import java.sql.SQLException;
 import java.util.function.BiConsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
 import ch.kalunight.zoe.command.ZoeCommand;
+import ch.kalunight.zoe.model.config.ServerConfiguration;
 import ch.kalunight.zoe.model.dto.DTO;
-import ch.kalunight.zoe.repositories.InfoChannelRepository;
+import ch.kalunight.zoe.repositories.ConfigRepository;
 import ch.kalunight.zoe.repositories.RankHistoryChannelRepository;
 import ch.kalunight.zoe.translation.LanguageManager;
 import ch.kalunight.zoe.util.CommandUtil;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class CreateRankHistoryChannelCommand extends ZoeCommand {
+
+  private static final Logger logger = LoggerFactory.getLogger(CreateRankHistoryChannelCommand.class);
 
   public CreateRankHistoryChannelCommand() {
     this.name = "rankChannel";
@@ -26,13 +33,13 @@ public class CreateRankHistoryChannelCommand extends ZoeCommand {
     this.help = "createRankChannelHelpMessage";
     this.helpBiConsumer = CommandUtil.getHelpMethodIsChildren(CreateCommand.USAGE_NAME, name, arguments, help);
   }
-  
+
+
   @Override
   protected void executeCommand(CommandEvent event) throws SQLException {
-    event.getTextChannel().sendTyping().complete();
 
     DTO.Server server = getServer(event.getGuild().getIdLong());
-    
+
     String nameChannel = event.getArgs();
 
     if(nameChannel == null || nameChannel.equals("")) {
@@ -44,23 +51,40 @@ public class CreateRankHistoryChannelCommand extends ZoeCommand {
       event.reply(LanguageManager.getText(server.serv_language, "nameOfTheInfoChannelNeedToBeLess100Characters"));
       return;
     }
-    
+
     DTO.RankHistoryChannel rankChannelDb = RankHistoryChannelRepository.getRankHistoryChannel(event.getGuild().getIdLong());
-    
+
     if(rankChannelDb != null && rankChannelDb.rhChannel_channelId != 0) {
       TextChannel rankChannel = event.getGuild().getTextChannelById(rankChannelDb.rhChannel_channelId);
       if(rankChannel == null) {
-        InfoChannelRepository.deleteInfoChannel(server);
+        RankHistoryChannelRepository.deleteRankHistoryChannel(rankChannelDb.rhChannel_id);
       }else {
         event.reply(String.format(LanguageManager.getText(server.serv_language, "rankChannelAlreadyExist"), rankChannel.getAsMention()));
         return;
       }
     }
+
+    TextChannel rankChannel = event.getGuild().createTextChannel(nameChannel).complete();
+
+    ServerConfiguration serverConfiguration = ConfigRepository.getServerConfiguration(event.getGuild().getIdLong());
+
+    if(serverConfiguration.getZoeRoleOption().getRole() != null) {
+      Role role = event.getGuild().getPublicRole();
+      try {
+        rankChannel.putPermissionOverride(role).deny(Permission.MESSAGE_READ, Permission.MESSAGE_HISTORY).queue();
+        rankChannel.putPermissionOverride(serverConfiguration.getZoeRoleOption().getRole()).grant(Permission.MESSAGE_READ, Permission.MESSAGE_HISTORY).queue();
+      }catch(Exception e) {
+        logger.info("Missing permission to apply rank channel rule, nothing bad", e);
+      }
+    }
+    
+    RankHistoryChannelRepository.createRankHistoryChannel(event.getGuild().getIdLong(), rankChannel.getIdLong());
+    
+    event.reply(LanguageManager.getText(server.serv_language, "rankChannelCorrectlyCreated"));
   }
 
   @Override
   public BiConsumer<CommandEvent, Command> getHelpBiConsumer(CommandEvent event) {
     return helpBiConsumer;
   }
-
 }

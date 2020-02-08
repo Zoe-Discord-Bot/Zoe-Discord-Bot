1b2d38a9eabdbeb20369ba60bc9a01dc7ed9b824
KaluNight <lucadasilva003@gmail.com>
2020-02-08 15:11:00 +0100

    Create deleteRankChannel Command

--- src/main/java/ch/kalunight/zoe/command/delete/DeleteRankHistoryChannelCommand.java
+++ src/main/java/ch/kalunight/zoe/command/delete/DeleteRankHistoryChannelCommand.java
@@ -2,25 +2,29 @@ package ch.kalunight.zoe.command.delete;
 
 import java.sql.SQLException;
 import java.util.function.BiConsumer;
+
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+
 import ch.kalunight.zoe.command.ZoeCommand;
 import ch.kalunight.zoe.model.dto.DTO;
 import ch.kalunight.zoe.repositories.InfoChannelRepository;
+import ch.kalunight.zoe.repositories.RankHistoryChannelRepository;
 import ch.kalunight.zoe.translation.LanguageManager;
 import ch.kalunight.zoe.util.CommandUtil;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 
-public class DeleteInfoChannelCommand extends ZoeCommand {
+public class DeleteRankHistoryChannelCommand extends ZoeCommand {
 
-  public DeleteInfoChannelCommand() {
-    this.name = "infoChannel";
+  public DeleteRankHistoryChannelCommand() {
+    this.name = "rankChannel";
     this.arguments = "";
-    this.help = "deleteInfoChannelHelpMessage";
     Permission[] permissionRequired = {Permission.MANAGE_CHANNEL};
     this.userPermissions = permissionRequired;
+    this.guildOnly = true;
+    this.help = "deleteRankChannelHelpMessage";
     this.helpBiConsumer = CommandUtil.getHelpMethodIsChildren(DeleteCommand.USAGE_NAME, name, arguments, help);
   }
 
@@ -29,24 +33,24 @@ public class DeleteInfoChannelCommand extends ZoeCommand {
 
     DTO.Server server = getServer(event.getGuild().getIdLong());
 
-    DTO.InfoChannel infochannel = InfoChannelRepository.getInfoChannel(server.serv_guildId);
+    DTO.RankHistoryChannel rankChannel = RankHistoryChannelRepository.getRankHistoryChannel(server.serv_guildId);
 
-    if(infochannel == null) {
-      event.reply(LanguageManager.getText(server.serv_language, "deleteInfoChannelChannelNotSetted"));
+    if(rankChannel == null) {
+      event.reply(LanguageManager.getText(server.serv_language, "deleteRankChannelNotSetted"));
     } else {
       try {
-        TextChannel textChannel = event.getGuild().getTextChannelById(infochannel.infochannel_channelid);
+        TextChannel textChannel = event.getGuild().getTextChannelById(rankChannel.rhChannel_channelId);
         if(textChannel != null) {
           textChannel.delete().queue();
         }
       } catch(InsufficientPermissionException e) {
         InfoChannelRepository.deleteInfoChannel(server);
-        event.reply(LanguageManager.getText(server.serv_language, "deleteInfoChannelDeletedMissingPermission"));
+        event.reply(LanguageManager.getText(server.serv_language, "deleteRankChannelDeletedMissingPermission"));
         return;
       }
 
       InfoChannelRepository.deleteInfoChannel(server);
-      event.reply(LanguageManager.getText(server.serv_language, "deleteInfoChannelDoneMessage"));
+      event.reply(LanguageManager.getText(server.serv_language, "deleteRankChannelDoneMessage"));
     }
   }
 
@@ -54,4 +58,5 @@ public class DeleteInfoChannelCommand extends ZoeCommand {
   public BiConsumer<CommandEvent, Command> getHelpBiConsumer(CommandEvent event) {
     return helpBiConsumer;
   }
+
 }

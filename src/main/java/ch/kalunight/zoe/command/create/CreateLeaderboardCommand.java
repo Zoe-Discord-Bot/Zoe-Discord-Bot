0e3ccba395d982b1206bc8a1eba5b1cd20fe9e44
KaluNight <lucadasilva003@gmail.com>
2020-05-11 08:44:28 +0200

    Start implement

--- src/main/java/ch/kalunight/zoe/command/create/CreateLeaderboardCommand.java
+++ src/main/java/ch/kalunight/zoe/command/create/CreateLeaderboardCommand.java
@@ -2,6 +2,8 @@ package ch.kalunight.zoe.command.create;
 
 import java.awt.Color;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -10,11 +12,13 @@ import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import com.jagrosh.jdautilities.menu.SelectionDialog;
 import ch.kalunight.zoe.command.ZoeCommand;
+import ch.kalunight.zoe.model.Objective;
 import ch.kalunight.zoe.model.dto.DTO;
 import ch.kalunight.zoe.translation.LanguageManager;
 import ch.kalunight.zoe.util.CommandUtil;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
+import net.rithms.riot.constant.Platform;
 
 public class CreateLeaderboardCommand extends ZoeCommand {
 
@@ -47,8 +51,20 @@ public class CreateLeaderboardCommand extends ZoeCommand {
         .setColor(Color.BLUE)
         .setSelectedEnds("**", "**")
         .setCanceled(getSelectionCancelAction(server.serv_language))
+        .setSelectionConsumer(getSelectionConsumer(server.serv_language))
         .setTimeout(2, TimeUnit.MINUTES);
     
+    List<Objective> objectiveList = new ArrayList<>();
+    List<String> objectiveChoices = new ArrayList<>();
+    for(Objective objective : Objective.values()) {
+      String actualChoice = String.format(LanguageManager.getText(server.serv_language, objective.toString()));
+      
+      objectiveChoices.add(actualChoice);
+      selectAccountBuilder.addChoices(actualChoice);
+      objectiveList.add(objective);
+    }
+    
+    selectAccountBuilder.setText(LanguageManager.getText(server.serv_language, "createLeaderboardTitleListeObjective"));
     
   }
 
@@ -58,6 +74,15 @@ public class CreateLeaderboardCommand extends ZoeCommand {
       public void accept(Message message) {
         message.clearReactions().queue();
         message.editMessage(LanguageManager.getText(language, "createLeaderboardCancelMessage")).queue();
+      }
+    };
+  }
+  
+  private BiConsumer<Message, Integer> getSelectionConsumer(String language) {
+    return new BiConsumer<Message, Integer>() {
+      @Override
+      public void accept(Message selectionMessage, Integer ObjectiveSelection) {
+        
       }
     };
   }
